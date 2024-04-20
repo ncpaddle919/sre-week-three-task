cefdcc6564f1beb7caf619b7fc116b98dddd650b
ncpaddle919 <dbwagoner@gmail.com>
2024-04-20 15:04:42 +0000

    added shell script

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,27 @@
-#TODO
+#!/bin/bash
+
+# Define Variables
+namespace="sre"
+deployment_name="sre"
+max_restarts=3
+
+# Start an Infinite Loop
+while true; do
+    # Check Pod Restarts
+    restarts=$(kubectl get pods -n $namespace | grep $deployment_name | awk '{print $4}')
+
+    # Display Restart Count
+    echo "Current number of restarts: $restarts"
+
+    # Check Restart Limit
+    if [[ $restarts -gt $max_restarts ]]; then
+        # Scale Down if Necessary
+        echo "Number of restarts exceeded maximum limit. Scaling down the deployment..."
+        kubectl scale deployment $deployment_name --replicas=0 -n $namespace
+        break
+    else
+        # Pause
+        echo "Pausing for 60 seconds..."
+        sleep 60
+    fi
+done
